c1d0f1ed6545f53564a7cda7c637ae8e644bc488
jesus <jelopezgonzalez@gmail.com>
2021-02-11 11:43:59 +0000

    CRUD usuario

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,8 +1,52 @@
 class UsuariosController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :find_usuario, only: [:show, :edit, :update, :delete]
 
   def index
+    @usuario = Usuario.all
+  end
+
+  def new
+    @usuario = Usuario.new
+  end
+
+  def create
+    @usuario = Usuario.create(strong_params_usuarios)
+    if @usuario.save
+      redirect_to usuarios_index_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @usuario.update(strong_params_usuarios)
+      redirect_to usuarios_index_path
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    @usuario.destroy
+    redirect_to usuarios_index_path
+  end
+
+  private
+
+  def find_usuario
+    @usuario = Usuario.find(params[:id])
+  end
+
+  def strong_params_usuarios
+    params.require(:usuario).permit(:tipo, :nombre, :apellidos, :pais, :email, :contraseña)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "usuarios#index"
+  root to: "usuarios#new"
 
   get 'usuarios/index', to: 'usuarios#index'
+  get 'usuarios/new', to: 'usuarios#new', as: :new_usuario
+  post "usuarios", to: "usuarios#create"
+  get 'usuarios/:id', to: "usuarios#show"
+  get 'usuarios/:id/edit', to: "usuarios#edit", as: :edit_usuario
+  patch "usuarios/:id", to: "usuarios#update", as: :usuario
+  delete "usuarios/:id", to: "usuarios#delete"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
